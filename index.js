ac009da3cd3b1201437530658c41e838cebf4c68
Satish-kumar-Meher <satishmeher.slr@gmail.com>
2026-02-04 02:02:07 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+const app = express();
+const PORT = 8080;
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello, World!' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is up and running on http://localhost:${PORT}`);
+});
